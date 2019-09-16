0188246ee862146882a5e0cf58667331e442a684
Dominic Sciascia <dominic@duhminick.com>
2019-09-16 01:19:47 -0400

    Cleaning up client and adding framework
    - Removed assets used from create-react-app.
    - Added Blueprint, a React UI framework by Palantir.
    - Added example code for Blueprint.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Navbar,
+  NavbarHeading,
+  NavbarGroup,
+  NavbarDivider,
+  Classes, 
+  Alignment,
+  AnchorButton} from '@blueprintjs/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Navbar className={Classes.DARK}>
+      <NavbarGroup align={Alignment.LEFT}>
+        <NavbarHeading>Baltimore Crime Viewer</NavbarHeading>
+        <NavbarDivider />
+        <AnchorButton href="#" text="Github" rightIcon="code" minimal />
+      </NavbarGroup>
+    </Navbar>
+  </div>
+)
 
 export default App;
