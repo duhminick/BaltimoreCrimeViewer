6b4babd2b955f8bc2d4338e4b5d49ecdd64fd9be
Dominic Sciascia <dominic@duhminick.com>
2019-09-19 01:11:15 -0400

    Added Apache Cassandra as database
    - Added Apache Cassandra to docker-compose.yml.
    - Added initialization script for Cassandra - WIP.

---
+++ database/cassandra-init.sh
@@ -0,0 +1,27 @@
+cat >/init.cql << EOF
+CREATE KEYSPACE IF NOT EXISTS bcv WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
+CREATE TABLE IF NOT EXISTS crime(
+    id int,
+    crimedate date,
+    crimehour int,
+    crimeminute int,
+    crimecode text,
+    location text,
+    secondarylocation text,
+    description text,
+    inside Boolean,
+    weapon text,
+    post text,
+    district text,
+    neighborhood text,
+    longitude double,
+    latitude double,
+    premise text,
+    PRIMARY KEY (id)
+);
+EOF
+
+until cqlsh -f /init.cql do
+    echo "cqlsh: cassandra is not yet available - retrying"
+    sleep 2
+done &
