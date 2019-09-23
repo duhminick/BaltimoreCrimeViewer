77977b42b808d46d9428be742f5e8237ec08d565
Dominic Sciascia <dominic@duhminick.com>
2019-09-23 13:38:44 -0400

    Added Redis to the project
    - Using Redis as our cache - configured with LRU eviction policy.
    - Retriever will periodically fetch now.
    - Duplicates are prevented from getting added to the database.

--- retriever/app.py
+++ retriever/app.py
@@ -1,5 +1,8 @@
 from sodapy import Socrata
 import psycopg2
+import redis
+import json
+import threading
 
 DATASET_PROVIDER = 'data.baltimorecity.gov'
 DATASET_IDENTIFIER = 'wsfq-mvij'
@@ -7,23 +10,42 @@ DATABASE_HOST = 'database'
 DATABASE_PORT = 5432
 DATABASE_NAME = 'bcv'
 DATABASE_USER = 'postgres'
+CACHE_HOST = 'cache'
+CACHE_PORT = 6379
+CACHE_DB = 0
+
+PERIOD = 60 * 60 # Run every hour
 
 # Connect to database
 db = psycopg2.connect(
-    host=DATABASE_HOST,
-    port=DATABASE_PORT,
-    dbname=DATABASE_NAME,
-    user=DATABASE_USER
+  host=DATABASE_HOST,
+  port=DATABASE_PORT,
+  dbname=DATABASE_NAME,
+  user=DATABASE_USER
 )
 cur = db.cursor()
+
+redis = redis.Redis(host=CACHE_HOST, port=CACHE_PORT, db=CACHE_DB)
+
+# Add in our schema
 with open('init.sql', 'r') as f:
-    cur.execute(f.read())
+  cur.execute(f.read())
 
 soc = Socrata(DATASET_PROVIDER, None)
 
-results = soc.get(DATASET_IDENTIFIER, limit=15)
-for result in results:
+def job():
+  results = soc.get(DATASET_IDENTIFIER, limit=5)
+  for result in results:
     if 'latitude' in result and 'longitude' in result:
-        cur.execute('INSERT INTO crimes (longitude, latitude) VALUES (%s, %s)', (float(result['longitude']), float(result['latitude'])))
-        print('Inserted')
-db.commit()
+      converted_to_string = json.dumps(result)
+      if not redis.exists(converted_to_string):
+        cur.execute('INSERT INTO crimes (longitude, latitude) VALUES (%s, %s)', (result['longitude'], result['latitude']))
+        redis.set(converted_to_string, 'true')
+        print('Inserted:', result['longitude'], result['latitude'])
+      else:
+        print('Already exists:', result['longitude'], result['latitude'])
+  db.commit()
+
+  threading.Timer(PERIOD, job).start()
+
+job()
