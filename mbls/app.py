69395b8f55897084a042be9dca2ba03ca920f813
Dominic Sciascia <dominic@duhminick.com>
2019-09-28 22:47:18 -0400

    Changed from Google Maps to Mapbox
    - Using react-map-gl form Uber.
    - Updated retriever to instead print the stats.
    - Added CORS to retriever.
    - Retriever now fetches 10000 rows then periodically fetches 2000 rows.
    - Heatmap now works!

--- mbls/app.py
+++ mbls/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS
 import database
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 db = database.get_instance()
