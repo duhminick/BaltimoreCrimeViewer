5e2448154da8971ec29b5e9072693a43029aae7b
Dominic Sciascia <dominic@duhminick.com>
2019-10-02 00:25:12 -0400

    Updated schema for the crimes table

--- retriever/init.sql
+++ retriever/init.sql
@@ -1,5 +1,17 @@
 CREATE TABLE IF NOT EXISTS crimes(
     id serial PRIMARY KEY,
     longitude double precision,
-    latitude double precision
+    latitude double precision,
+    crimedate date,
+    crimehour int,
+    crimeminute int,
+    crimecode text,
+    location text,
+    description text,
+    inside boolean,
+    weapon text,
+    post text,
+    district text,
+    neighborhood text,
+    premise text
 );
